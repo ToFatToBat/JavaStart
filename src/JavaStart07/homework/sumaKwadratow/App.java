75c7e4fe1f5ea7d5225be1709f07b3b0e9883840
ToFatToBat <kpapiernia@wp.pl>
2020-06-28 07:17:50 +0200

    Zadanie domowe

---
+++ src/JavaStart07/homework/sumaKwadratow/App.java
@@ -0,0 +1,31 @@
+package JavaStart07.homework.sumaKwadratow;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        int sumSquareNumber = 0;
+        int sumSquareNumberDifferentMethod = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Podaj wielkosc tablicy");
+        int tableSize = sc.nextInt();
+
+        int [] table = new int[tableSize];
+
+        for (int i =0; i< table.length; i++) {
+            System.out.println("Podaj " + i + " liczbe");
+            table[i] = sc.nextInt();
+            sumSquareNumber += (table[i] * table[i]);
+        }
+
+        System.out.println("Suma kwadratow liczb w tablicy to:" + sumSquareNumber);
+
+        for (int p: table) {
+            sumSquareNumberDifferentMethod += table[p-1] * table[p-1];
+            System.out.println("Liczba wprowadzona to " + table[p-1]);
+        }
+
+        System.out.println("Suma kwadratow liczb w tablicy 2 metoda to:" + sumSquareNumberDifferentMethod);
+    }
+}
