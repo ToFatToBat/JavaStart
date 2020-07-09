dc4d04980ae244de1720d5de9d3f0a88accc8702
ToFatToBat <kpapiernia@wp.pl>
2020-07-09 19:28:29 +0200

    Zadanie domowe

---
+++ src/JavaStart11/zajecia/piecLiczb/App.java
@@ -0,0 +1,38 @@
+package JavaStart11.zajecia.piecLiczb;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        int sum = 0;
+        int counter = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("podaj 5 liczb:");
+
+
+        do {
+            try {
+
+
+                System.out.println("Podaj kolejna liczbe: ");
+                int next = sc.nextInt();
+                sum += next;
+            } catch (InputMismatchException e) {
+                sc.nextInt();
+            }
+
+        } while (counter < 5);
+
+
+        System.out.println("POdales wszystkie liczby");
+
+
+        System.out.println("Suma liczb to: " + sum);
+
+
+    }
+}
+
