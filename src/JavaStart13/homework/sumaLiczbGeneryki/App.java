8c216a4b182bc08ce71acc8dbd09bf791ce433b0
ToFatToBat <kpapiernia@wp.pl>
2020-07-20 16:34:14 +0200

    Zadanie domowe

---
+++ src/JavaStart13/homework/sumaLiczbGeneryki/App.java
@@ -0,0 +1,32 @@
+package JavaStart13.homework.sumaLiczbGeneryki;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        List<Double> lista = new ArrayList<>();
+        double inputNumber = 0;
+
+
+        do {
+            System.out.println("Podaj liczbe:");
+            inputNumber = sc.nextDouble();
+            if (inputNumber>0) {
+                lista.add(inputNumber);
+            }
+
+        } while (inputNumber > 0);
+
+
+        for (Double element: lista){
+            System.out.println(element);
+        }
+        System.out.println(lista.toString());
+
+    }
+}
