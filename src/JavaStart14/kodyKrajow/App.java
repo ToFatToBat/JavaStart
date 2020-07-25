93f5fd3571c2d275982addbe0049e97eb532b368
ToFatToBat <kpapiernia@wp.pl>
2020-07-25 10:34:45 +0200

    Zadanie domowe

--- src/JavaStart14/kodyKrajow/App.java
+++ src/JavaStart14/kodyKrajow/App.java
@@ -10,43 +10,42 @@ public class App {
     public static void main(String[] args) {
 
 
-
-        Map<String, Country> countryMap1 =  dataUploadAndLoad();
-        String choosenKeySet = userChoise(countryMap1);
-        printChoise(countryMap1, choosenKeySet);
+        Map<String, Country> countryMap1 = dataUploadAndLoad();
+        String choosenKeySet = userChoice(countryMap1);
+        printChoice(countryMap1, choosenKeySet);
 
     }
 
-    private static void printChoise(Map<String, Country> countryMap1, String choosenKeySet) {
+    private static void printChoice(Map<String, Country> countryMap1, String choosenKeySet) {
         System.out.println(countryMap1.get(choosenKeySet));
     }
 
-    private static String userChoise(Map<String, Country> countryMap1) {
+    private static String userChoice(Map<String, Country> countryMap1) {
         System.out.println("Podaj jeden z indeksow w celu wyswietlenia pelnej informacji:");
         System.out.println(countryMap1.keySet());
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
 
-    private static Map<String, Country> dataUploadAndLoad () {
-
-        String filename = "C:\\Users\\kpapi\\JavaStart\\JavaStart\\src\\JavaStart14\\kodyKrajow\\countries.csv";
-        File file = new File(filename);
-        Scanner sc = null;
+    private static Map<String, Country> dataUploadAndLoad() {
+        Map<String, Country> countryMap = new HashMap<>();
         try {
+            String filename = "C:\\Users\\kpapi\\JavaStart\\JavaStart\\src\\JavaStart14\\kodyKrajow\\countries.csv";
+            File file = new File(filename);
+            Scanner sc = null;
+
             sc = new Scanner(file);
+
+            while (sc.hasNextLine()) {
+                String oneLine = sc.nextLine();
+                String[] oneLineTable = oneLine.split(";");
+                Country country = new Country(oneLineTable[0], oneLineTable[1], Integer.valueOf(oneLineTable[2]));
+                countryMap.put(oneLineTable[0], country);
+            }
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        Map<String, Country> countryMap = new HashMap<>();
-
-        while (sc.hasNextLine()) {
-            String oneLine = sc.nextLine();
-            String[] oneLineTable = oneLine.split(";");
-            Country country = new Country(oneLineTable[0], oneLineTable[1], Integer.valueOf(oneLineTable[2]));
-            countryMap.put(oneLineTable[0], country);
-        }
-
         return countryMap;
     }
 
